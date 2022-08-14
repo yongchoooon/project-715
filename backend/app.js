a9b89128e40f628d4739f5231ddc9a920f506ed8
buhyun <hwangbh8@naver.com>
2022-08-14 16:26:09 +0900

    module

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',   
-  password: '158746',
+  password: 'bh990731!',
   database: '715job'
 });  
 // Connect
